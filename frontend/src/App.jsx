bb5d6b14beadfd098ea77fca37636d90acdc146a
zidney06 <zidniimportant@gmail.com>
2025-04-22 15:06:06 +0700

    menambahkan redux dan axios kedala, project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,25 @@ import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import Navbar from './components/Navbar'
 import './styles/style.css'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import {setInitial} from '../slices/productSlice'
+import {useDispatch} from 'react-redux'
 
 function App() {
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		axios.get('http://localhost:3000/books')
+		.then((res) => {
+			dispatch(setInitial(res.data))
+			console.log(res)
+		})
+		.catch((e) => {
+			console.log(e)
+		})
+	}, [])
+
   return (
     <>
     	<Navbar />
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -8,8 +8,16 @@ import {
 	CardMedia,
 	Button
 } from '@mui/material'
+import {useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
 
 export default function ProductList ({header}) {
+	const books = useSelector((state) => state.books.value)
+
+	useEffect(() => {
+	  
+	}, [])
+
   return (
    <Container
    	component="section"
@@ -57,48 +65,3 @@ export default function ProductList ({header}) {
    </Container>
   )
 }
-
-const books = [
-	{
-		title: 'Buku Sejarah',
-		author: 'Atin',
-		price: 12000,
-		ISBN: '123-234-345',
-		image: "https://images.unsplash.com/photo-1476275466078-4007374efbbe?q=80&w=829&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-	},
-	{
-		title: 'Buku Tulis',
-		author: 'Mia ni',
-		price: 17500,
-		ISBN: '123-234-345',
-		image: "https://media.istockphoto.com/id/1180068638/photo/open-book-on-autumn-grass-and-fallen-leaves.webp?a=1&s=612x612&w=0&k=20&c=nXxQurwM2YNEWkRU2fwq0H0WZJRusdnyEpnpI0Poxd4="
-	},
-	{
-		title: 'Buku Sosial',
-		author: 'Andik',
-		price: 23000,
-		ISBN: '123-234-345',
-		image: "https://images.unsplash.com/photo-1517770413964-df8ca61194a6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-	},
-	{
-		title: 'Buku Sejarah',
-		author: 'Atin',
-		price: 12000,
-		ISBN: '123-234-345',
-		image: "https://images.unsplash.com/photo-1476275466078-4007374efbbe?q=80&w=829&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-	},
-	{
-		title: 'Buku Tulis',
-		author: 'Mia ni',
-		price: 17500,
-		ISBN: '123-234-345',
-		image: "https://media.istockphoto.com/id/1180068638/photo/open-book-on-autumn-grass-and-fallen-leaves.webp?a=1&s=612x612&w=0&k=20&c=nXxQurwM2YNEWkRU2fwq0H0WZJRusdnyEpnpI0Poxd4="
-	},
-	{
-		title: 'Buku Sosial',
-		author: 'Andik',
-		price: 23000,
-		ISBN: '123-234-345',
-		image: "https://images.unsplash.com/photo-1517770413964-df8ca61194a6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-	}
-]
